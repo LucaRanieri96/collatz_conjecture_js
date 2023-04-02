15861de414cf1c74fc633b50263c0a1982b38bc8
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-04-02 11:52:07 +0200

    Add collatzconjecture function

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,27 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+      inputNumber: "",
       result: "",
     };
   },
   methods: {
-   
-  },
+    CollatzConjecture: () => {
+      let n = parseInt(this.inputNumber);
+
+      
+      let number = 0;
+      while (n !== 1) {
+        if (n % 2 === 0) {
+          n = n / 2;
+        } else {
+          n = (3 * n) + 1;
+        }
+        number++;
+      }
+      this.result = number;
+    }
+  }
 }).mount("#app");
 
 // add event listener v-on @ sul button
